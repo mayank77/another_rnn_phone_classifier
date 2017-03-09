257d13119299984f4a66b86f2e57b4e0ff2d8e11
Reima Karhila <reima.karhila@aalto.fi>
2017-03-09 11:48:15 +0200

    serving runs, but does it work?

--- serving/serve.sh
+++ serving/serve.sh
@@ -3,9 +3,9 @@
 
 PATH=/l/opt/anaconda3/bin:/usr/local/bin/:$PATH
 
-source activate tensorflow3
+source activate tensorflow012
 
-python ./serve_bidir_lstm_384x6.py \
+python3 ./serve_bidir_lstm_384x6.py \
     /home/condadnn/another_rnn_phone_classifier/serving/ports/portnr \
     /home/condadnn/another_rnn_phone_classifier/models/bidir_lstm_6x384_checkpoint_16000/model2.ckpt-16000 \
     /home/condadnn/another_rnn_phone_classifier/models/bidir_lstm_6x384_checkpoint_16000/traindata.mean \
